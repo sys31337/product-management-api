f5d01f32aaaf58e6f2ed44f421011e2910072df4
sys31337 <paytf@outlook.com>
2024-10-01 23:12:25 +0100

    Update users.ts

--- src/routes/helpers/users.ts
+++ src/routes/helpers/users.ts
@@ -1,14 +1,12 @@
 import express from 'express';
-import auth from '@middlewares/auth';
-import { createAccount, login, refreshToken } from '@controllers/users';
+import { createAccount, login, logout, refreshToken } from '@controllers/users';
 import { loginValidator, registerValidator } from '@validations/users';
 
 const router = express.Router();
 
-router.route('/')
-  .post(registerValidator, createAccount);
-
-router.post('/auth', loginValidator, login)
-router.post('/refresh', refreshToken)
+router.post('/', registerValidator, createAccount);
+router.post('/auth', loginValidator, login);
+router.post('/refresh', refreshToken);
+router.post('/logout', logout);
 
 export default router;
